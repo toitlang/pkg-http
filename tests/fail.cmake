805450d2051fd286759b190fa8c3dbc18ff38d89
Florian Loitsch <florian@loitsch.com>
2022-06-15 13:35:22 +0200

    Run tests on all platforms.
    Fail on windows.

---
+++ tests/fail.cmake
@@ -0,0 +1,12 @@
+# Copyright (C) 2022 Toitware ApS.
+# Use of this source code is governed by a Zero-Clause BSD license that can
+# be found in the tests/LICENSE file.
+
+set(FAILING_TESTS
+)
+
+if ("${CMAKE_SYSTEM_NAME}" STREQUAL "Windows" OR "${CMAKE_SYSTEM_NAME}" STREQUAL "MSYS")
+  list(APPEND FAILING_TESTS
+    ${TEST_PREFIX}/tests/redirect_test.toit
+  )
+endif()
